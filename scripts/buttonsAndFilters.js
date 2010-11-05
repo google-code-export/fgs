7486be09d896896765cc8e148c8a042cc8984f4e
a.rzadkowolski@gmail.com <a.rzadkowolski@gmail.com@6ef512da-1914-2133-09cd-fb00ea706427>
2010-11-05 22:21:17 +0000

    version 3.6.6

---
+++ scripts/buttonsAndFilters.js
@@ -0,0 +1,284 @@
+var gamesData = 
+{
+	201278444497: 	{ 	
+						name: 'FrontierVille',	systemName: 'frontierville',
+						filter:		
+						{	
+							bonuses: ['Go see my gift', 'Go Play FrontierVille', 'Go to FrontierVille', 'Send items'],		
+							bonusesNewWindow: [],
+							requests: ['r4r'],
+							requestsNewWindow: [],
+						},
+						buttons:	
+						{
+							'BonusesPending': 	{	name: 'Show pending bonuses (<span>?</span>)',
+													submenu: {
+																'ReceivePendingBonuses': { name: 'Receive all bonuses' },
+																'ClearPendingBonuses': 	{ name: 'Clear all bonuses' },
+															}
+												},
+							'ManualBonuses':	{	name: 'Bonuses With Action Required (<span>?</span>)',
+													submenu: {
+																'ClearManualBonuses': { name: 'Clear all bonuses' },
+															}
+												},
+							'BonusesHistory': 	{	name: 'Show collected bonuses',
+													submenu: {
+																'ClearCollectedBonuses': { name: 'Clear bonuses history' }
+															}
+												},
+							'RequestsPending':	{	name: 'Show pending gifts (<span>?</span>)',
+													submenu: {
+																'ReceivePendingRequests': { name: 'Receive all gifts' },
+																'ClearPendingRequests': 	{ name: 'Clear all requests' },
+															}
+												}, 
+							'RequestsHistory':	{	name: 'Show collected gifts',
+													submenu: {
+																'ClearCollectedRequests': { name: 'Clear gifts history' },
+															}
+												}, 
+							'SendFreeGifts':	{	name: 'Send free gifts',
+													submenu: {
+																'SelectFavourites': { name: 'Select favourites' },
+																'SendGifts': { name: 'Send free gifts' },
+															}
+												}, 
+							'SendFreeGiftsHistory':	{	name: 'Show free gifts history',
+													submenu: {
+																'ClearSentFreeGifts': { name: 'Clear send gifts history' },
+															}
+												},
+							'GameOptionsTab':	{	name: 'Filters',
+							},
+						}
+					},
+	102452128776: 	{ 	
+						name: 'FarmVille',		systemName: 'farmville',
+						filter:
+						{	
+							bonuses: ['mission=fertilize', 'next=gifts\.php'],
+							bonusesNewWindow: [],
+							requests: ['Toolbar_Install', 'sendcredits'],
+							requestsNewWindow: [],
+						},
+						buttons:	
+						{
+							'BonusesPending': 	{	name: 'Show pending bonuses (<span>?</span>)',
+													submenu: {
+																'ReceivePendingBonuses': { name: 'Receive all bonuses' },
+																'ClearPendingBonuses': 	{ name: 'Clear all bonuses' },
+															}
+												},
+							'ManualBonuses':	{	name: 'Bonuses With Action Required (<span>?</span>)',
+													submenu: {
+																'ClearManualBonuses': { name: 'Clear all bonuses' },
+															}
+												},
+							'BonusesHistory': 	{	name: 'Show collected bonuses',
+													submenu: {
+																'ClearCollectedBonuses': { name: 'Clear bonuses history' }
+															}
+												},
+							'RequestsPending':	{	name: 'Show pending gifts (<span>?</span>)',
+													submenu: {
+																'ReceivePendingRequests': { name: 'Receive all gifts' },
+																'ClearPendingRequests': 	{ name: 'Clear all requests' },
+															}
+												}, 
+							'RequestsHistory':	{	name: 'Show collected gifts',
+													submenu: {
+																'ClearCollectedRequests': { name: 'Clear gifts history' },
+															}
+												},
+							'SendFreeGifts':	{	name: 'Send free gifts',
+													submenu: {
+																'SelectFavourites': { name: 'Select favourites' },
+																'SendGifts': { name: 'Send free gifts' },
+															}
+												}, 
+							'SendFreeGiftsHistory':	{	name: 'Show free gifts history',
+													submenu: {
+																'ClearSentFreeGifts': { name: 'Clear send gifts history' },
+															}
+												},
+							'GameOptionsTab':	{	name: 'Filters',
+							},
+						}
+					},
+	10979261223: 	{ 	
+						name: 'Mafia Wars',		systemName: 'mafiawars',
+						filter:		
+						{	
+							bonuses: ['Play Mafia Wars', 'Send Mystery Bag', 'Send Satchel of Lira'],		
+							bonusesNewWindow: ['^Help','Play Slots'],
+							requests: [],
+							requestsNewWindow: [
+								'Join me on a crime spree and find out what we loot',
+								'needs your help to complete an important mission',
+								'needs your skills as (.*) to complete an important mission',
+							],
+						},
+						buttons:	
+						{
+							'BonusesPending': 	{	name: 'Show pending bonuses (<span>?</span>)',
+													submenu: {
+																'ReceivePendingBonuses': { name: 'Receive all bonuses' },
+																'ClearPendingBonuses': 	{ name: 'Clear all bonuses' },
+															}
+												},
+							'ManualBonuses':	{	name: 'Bonuses With Action Required (<span>?</span>)',
+													submenu: {
+																'ClearManualBonuses': { name: 'Clear all bonuses' },
+															}
+												},
+							'BonusesHistory': 	{	name: 'Show collected bonuses',
+													submenu: {
+																'ClearCollectedBonuses': { name: 'Clear bonuses history' }
+															}
+												},
+							'RequestsPending':	{	name: 'Show pending gifts (<span>?</span>)',
+													submenu: {
+																'ReceivePendingRequests': { name: 'Receive all gifts' },
+																'ClearPendingRequests': 	{ name: 'Clear all requests' },
+															}
+												}, 
+							'RequestsHistory':	{	name: 'Show collected gifts',
+													submenu: {
+																'ClearCollectedRequests': { name: 'Clear gifts history' },
+															}
+												},
+							'GameOptionsTab':	{	name: 'Filters',
+							},
+						}
+					},
+	234860566661: 	{ 	
+						name: 'Treasure Isle',		systemName: 'treasure',
+						filter:		
+						{	
+							bonuses: ['Play Treasure Isle'],	
+							bonusesNewWindow: [],							
+							requests: [],
+							requestsNewWindow: [],
+						},
+						buttons:	
+						{
+							'BonusesPending': 	{	name: 'Show pending bonuses (<span>?</span>)',
+													submenu: {
+																'ReceivePendingBonuses': { name: 'Receive all bonuses' },
+																'ClearPendingBonuses': 	{ name: 'Clear all bonuses' },
+															}
+												},
+							'ManualBonuses':	{	name: 'Bonuses With Action Required (<span>?</span>)',
+													submenu: {
+																'ClearManualBonuses': { name: 'Clear all bonuses' },
+															}
+												},
+							'BonusesHistory': 	{	name: 'Show collected bonuses',
+													submenu: {
+																'ClearCollectedBonuses': { name: 'Clear bonuses history' }
+															}
+												},
+							'RequestsPending':	{	name: 'Show pending gifts (<span>?</span>)',
+													submenu: {
+																'ReceivePendingRequests': { name: 'Receive all gifts' },
+																'ClearPendingRequests': 	{ name: 'Clear all requests' },
+															}
+												}, 
+							'RequestsHistory':	{	name: 'Show collected gifts',
+													submenu: {
+																'ClearCollectedRequests': { name: 'Clear gifts history' },
+															}
+												},
+							'GameOptionsTab':	{	name: 'Filters',
+							},
+						}
+					},
+	101539264719: 	{ 
+						name: 'Cafe World',		systemName: 'cafeworld',
+						filter:		
+						{
+							bonuses: ['Get Lotto Ticket', '^Visit', '^Help ', 'Send Energy'],
+							bonusesNewWindow: [],
+							requests: [],
+							requestsNewWindow: []
+						},
+						buttons:	
+						{
+							'BonusesPending': 	{	name: 'Show pending bonuses (<span>?</span>)',
+													submenu: {
+																'ReceivePendingBonuses': { name: 'Receive all bonuses' },
+																'ClearPendingBonuses': 	{ name: 'Clear all bonuses' },
+															}
+												},
+							'ManualBonuses':	{	name: 'Bonuses With Action Required (<span>?</span>)',
+													submenu: {
+																'ClearManualBonuses': { name: 'Clear all bonuses' },
+															}
+												},
+							'BonusesHistory': 	{	name: 'Show collected bonuses',
+													submenu: {
+																'ClearCollectedBonuses': { name: 'Clear bonuses history' }
+															}
+												},
+							'RequestsPending':	{	name: 'Show pending gifts (<span>?</span>)',
+													submenu: {
+																'ReceivePendingRequests': { name: 'Receive all gifts' },
+																'ClearPendingRequests': 	{ name: 'Clear all requests' },
+															}
+												}, 
+							'RequestsHistory':	{	name: 'Show collected gifts',
+													submenu: {
+																'ClearCollectedRequests': { name: 'Clear gifts history' },
+															}
+												},
+							'GameOptionsTab':	{	name: 'Filters',
+							},
+						}
+					},
+	120563477996213: 	{ 
+						name: 'Ravenwood Fair',		systemName: 'ravenwood',
+						filter:		
+						{
+							bonuses: [],
+							bonusesNewWindow: ['(.*)'],
+							requests: [],
+							requestsNewWindow: []
+						},
+						buttons:	
+						{
+							/*'BonusesPending': 	{	name: 'Show pending bonuses (<span>?</span>)',
+													submenu: {
+																'ReceivePendingBonuses': { name: 'Receive all bonuses' },
+																'ClearPendingBonuses': 	{ name: 'Clear all bonuses' },
+															}
+												},
+							*/
+							'ManualBonuses':	{	name: 'Bonuses With Action Required (<span>?</span>)',
+													submenu: {
+																'ClearManualBonuses': { name: 'Clear all bonuses' },
+															}
+												},
+							/*
+							'BonusesHistory': 	{	name: 'Show collected bonuses',
+													submenu: {
+																'ClearCollectedBonuses': { name: 'Clear bonuses history' }
+															}
+												},
+							*/
+							'RequestsPending':	{	name: 'Show pending gifts (<span>?</span>)',
+													submenu: {
+																'ReceivePendingRequests': { name: 'Receive all gifts' },
+																'ClearPendingRequests': 	{ name: 'Clear all requests' },
+															}
+												}, 
+							'RequestsHistory':	{	name: 'Show collected gifts',
+													submenu: {
+																'ClearCollectedRequests': { name: 'Clear gifts history' },
+															}
+												},
+							'GameOptionsTab':	{	name: 'Filters',
+							},
+						}
+					},
+};
---
+++ scripts/games/farmville.js
@@ -0,0 +1,253 @@
+var farmvilleRequests = 
+{	
+	APPID: '102452128776',
+	
+	Click: function(id, dataPost, retry)
+	{
+		var info = {
+			image: 'gfx/90px-cancel.png'
+		}
+		
+		$.ajax({
+			type: "POST",
+			url: 'http://www.facebook.com/ajax/reqs.php?__a=1',
+			data: dataPost,
+			dataType: 'text',
+			success: function(data)
+			{
+				try
+				{
+					var strTemp = data;
+					var i1      =   strTemp.indexOf('goURI');
+					if (i1 == -1) throw {message:"Cannot find goURI in page"}
+
+					var i2        =   strTemp.indexOf(');"]',i1);
+					strTemp   =   "'"+strTemp.slice(i1+6,i2)+"'";
+
+					eval("strTemp =" + strTemp);
+
+					var URI = JSON.parse(strTemp);
+					
+					$.ajax({
+						type: "GET",
+						url: URI,
+						dataType: 'text',
+						success: function(data2)
+						{
+							var data = data2.substr(data2.indexOf('<body'),data2.lastIndexOf('</body'));
+							
+							var i1 = URI.indexOf('addneighbo');
+							
+							if(i1 != -1)
+							{
+								console.log('New neighbour');
+
+								info.image = 'icons/reqs/noimage.png';
+								info.title = 'New neighbour';
+								info.text  = '';
+								info.time = Math.round(new Date().getTime() / 1000);
+																
+								database.updateItem('requests', id, info);
+								sendView('requestSuccess', id, info);
+								return;
+							}
+
+							if($('.giftFrom_img', data).length > 0 && $(".giftConfirm_img",data).length > 0)
+							{
+								//gift sukces
+								console.log('New gift');
+								
+								info.image = $(".giftConfirm_img",data).children().attr("src");
+								info.title = $(".giftConfirm_name",data).children().text();
+								info.text  = $(".giftFrom_name",data).children().text();
+								info.time = Math.round(new Date().getTime() / 1000);
+								
+								database.updateItem('requests', id, info);
+								sendView('requestSuccess', id, info);
+							}
+							else if($('.giftFrom_img', data).length == 0 && $(".giftConfirm_img",data).length > 0)
+							{
+								//gift sukces
+								console.log('New gift sent');
+								
+								info.image = $(".giftConfirm_img",data).children().attr("src");
+								info.title = $(".giftConfirm_name",data).children().text();
+								info.text  = $(".padding_content",data).find('h3').text();
+								info.time = Math.round(new Date().getTime() / 1000);
+								
+								database.updateItem('requests', id, info);
+								sendView('requestSuccess', id, info);
+							}
+							else if($('.giftLimit', data).length > 0)
+							{
+								info.error = 'limit';
+								info.time = Math.round(new Date().getTime() / 1000);
+								
+								database.updateErrorItem('requests', id, info);
+								sendView('requestError', id, info);
+							}
+							else
+							{
+								if(typeof(retry) == 'undefined')
+								{
+									console.log(getCurrentTime()+'[B] Connection error while receiving bonus, Retrying bonus with ID: '+id);
+									farmvilleRequests.Click(id, dataPost, true);
+								}
+								else
+								{
+									info.error = 'receiving';
+									info.time = Math.round(new Date().getTime() / 1000);
+									
+									database.updateErrorItem('requests', id, info);
+									sendView('requestError', id, info);	
+								}
+							}
+						},
+						error: function()
+						{
+							if(typeof(retry) == 'undefined')
+							{
+								console.log(getCurrentTime()+'[R] Connection error while receiving bonus, Retrying bonus with ID: '+id);
+								farmvilleRequests.Click(id, dataPost, true);
+							}
+							else
+							{
+								info.error = 'connection';
+								info.time = Math.round(new Date().getTime() / 1000);
+								sendView('requestError', id, info);
+							}
+						}
+					});
+				}
+				catch(err)
+				{
+					console.log(err);
+					if(typeof(retry) == 'undefined')
+					{
+						console.log(getCurrentTime()+'[B] Connection error while receiving bonus, Retrying bonus with ID: '+id);
+						farmvilleRequests.Click(id, dataPost, true);
+					}
+					else
+					{
+						info.error = 'receiving';
+						info.time = Math.round(new Date().getTime() / 1000);
+						
+						database.updateErrorItem('requests', id, info);
+						sendView('requestError', id, info);	
+					}
+				}
+			},
+			error: function()
+			{
+				if(typeof(retry) == 'undefined')
+				{
+					console.log(getCurrentTime()+'[R] Connection error while receiving bonus, Retrying bonus with ID: '+id);
+					farmvilleRequests.Click(id, dataPost, true);
+				}
+				else
+				{
+					info.error = 'connection';
+					info.time = Math.round(new Date().getTime() / 1000);
+					sendView('requestError', id, info);
+				}
+			}
+		});
+	}
+};
+
+
+var farmvilleBonuses = 
+{
+	APPID: '102452128776',
+
+	Click:	function(id, url, retry)
+	{
+		var info = {
+			image: 'gfx/90px-cancel.png'
+		}
+	
+		console.log(getCurrentTime()+'[B] Receiving bonus with ID: '+id);
+		
+		$.ajax({
+			type: "GET",
+			url: url,
+			success: function(data)
+			{
+					data = data.substr(data.indexOf('<body'),data.lastIndexOf('</body'));
+
+					if($('.inputsubmit[value="OK"]',data).length > 0)
+					{
+						console.log(getCurrentTime()+'[B] Bonus already claimed - deleting bonus with ID: '+id);
+						
+						info.error = 'limit';
+						info.time = Math.round(new Date().getTime() / 1000);
+						
+						database.updateErrorItem('bonuses', id, info);
+						sendView('bonusError', id, info);						
+					}
+					else
+					{
+					
+						url = unescape(url);
+						url = url.substr(url.indexOf('next')+5);
+						var giftReceiveUrl = 'http://apps.facebook.com/onthefarm/'+url;
+						
+						//if(options.games[farmvilleBonuses.APPID].sendGiftBack) { var num = 3; }	else { var num = 1; }
+						
+						//if($('input[name="sendThankYouGift"]', data).length == 0)
+						//{
+							var num = 1;
+						//}
+
+						var giftReceivePost = $('.inner_giftConfirm_cont', data).find('form:nth-child('+num+')').serialize()+'&'+escape($('.inner_giftConfirm_cont', data).find('form:nth-child('+num+')').find('input[type="submit"]').attr('name'))+'='+$('.inner_giftConfirm_cont', data).find('form:nth-child('+num+')').find('input[type="submit"]').attr('value');
+						
+						info.title = $(".giftConfirm_name",data).children().text();
+						info.image = $(".giftConfirm_img",data).children().attr("src");
+						info.text  = $(".main_giftConfirm_cont", data).find('h3').text();
+						
+						$.ajax({
+							type: "POST",
+							data: giftReceivePost,
+							url: giftReceiveUrl,
+							success: function(d)
+							{
+								info.time = Math.round(new Date().getTime() / 1000);
+								
+								database.updateItem('bonuses', id, info);
+								sendView('bonusSuccess', id, info);
+								console.log(getCurrentTime()+'[B] Bonus collected SUCCESSFULLY - ID: '+id);
+							},
+							error: function()
+							{
+								if(typeof(retry) == 'undefined')
+								{
+									console.log(getCurrentTime()+'[B] Connection error while receiving bonus, Retrying bonus with ID: '+id);
+									farmvilleBonuses.Click(id, url, true);
+								}
+								else
+								{
+									info.error = 'connection';
+									info.time = Math.round(new Date().getTime() / 1000);
+									sendView('bonusError', id, info);
+								}
+							}
+						});
+					}		
+			},
+			error: function()
+			{
+				if(typeof(retry) == 'undefined')
+				{
+					console.log(getCurrentTime()+'[B] Connection error while receiving bonus, Retrying bonus with ID: '+id);
+					farmvilleBonuses.Click(id, url, true);
+				}
+				else
+				{
+					info.error = 'connection';
+					info.time = Math.round(new Date().getTime() / 1000);
+					sendView('bonusError', id, info);
+				}
+			}
+		});
+	},
+};
