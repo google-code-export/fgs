dba2ea60a0ef0454cb6a03f85a1923c75060f539
a.rzadkowolski@gmail.com <a.rzadkowolski@gmail.com@6ef512da-1914-2133-09cd-fb00ea706427>
2011-07-05 15:22:51 +0000

    fix ravenwood.js

--- scripts/games/ravenwood.js
+++ scripts/games/ravenwood.js
@@ -88,10 +88,8 @@ FGS.ravenwood.Freegifts =
 			{
 				try
 				{
-					var pos1 = dataStr.indexOf('fbparams: "');
-					var pos2 = dataStr.indexOf('"', pos1+11);
-					params.step3params = 'ask_gift=-1&item_id='+params.gift+'&recipient_id=&thankyou_gift=0&default_tab=game&'+unescape(dataStr.slice(pos1+11, pos2));
-					params.step3url = 'http://www.ravenwoodfair.com/app/1/gift/send';
+					params.step3params = 'cachebuster='+Math.random()+'&filter=existing&ask_gift=-1&item_id='+params.gift+'&recipient_id=&thankyou_gift=0&default_tab=game&request_ids=&gift_icon=&'+params.step2params;
+					params.step3url = 'http://fbcanvas.ravenwoodfair.com/app/1/gift/send';
 					
 					FGS.ravenwood.Freegifts.Click3(params);
 				}
@@ -151,69 +149,6 @@ FGS.ravenwood.Freegifts =
 			{
 				try
 				{
-					var pos1 = dataStr.lastIndexOf('fb:tab-item href="');
-					var pos2 = dataStr.indexOf('"', pos1+18);
-					params.step4url = dataStr.slice(pos1+18, pos2);
-				
-					FGS.ravenwood.Freegifts.Click4(params);
-				
-				}
-				catch(err)
-				{
-					FGS.dump(err);
-					FGS.dump(err.message);
-					if(typeof(retry) == 'undefined')
-					{
-						retryThis(params, true);
-					}
-					else
-					{
-						if(typeof(params.sendTo) == 'undefined')
-						{
-							FGS.sendView('updateNeighbors', false, params.gameID);
-						}
-						else
-						{
-							FGS.sendView('errorWithSend', params.gameID, (typeof(params.thankYou) != 'undefined' ? params.bonusID : '') );
-						}
-					}
-				}
-			},
-			error: function()
-			{
-				if(typeof(retry) == 'undefined')
-				{
-					retryThis(params, true);
-				}
-				else
-				{
-					if(typeof(params.sendTo) == 'undefined')
-					{
-						FGS.sendView('updateNeighbors', false, params.gameID);
-					}
-					else
-					{
-						FGS.sendView('errorWithSend', params.gameID, (typeof(params.thankYou) != 'undefined' ? params.bonusID : '') );
-					}
-				}
-			}
-		});
-	},
-	Click4: function(params, retry)
-	{
-		var $ = FGS.jQuery;
-		var retryThis 	= arguments.callee;
-		var addAntiBot = (typeof(retry) == 'undefined' ? '' : '');
-
-		$.ajax({
-			type: "POST",
-			url: params.step3url+addAntiBot,
-			data: params.step3params,
-			dataType: 'text',
-			success: function(dataStr)
-			{
-				try
-				{
 					var tst = new RegExp(/(<fb:fbml[^>]*?[\s\S]*?<\/fb:fbml>)/m).exec(dataStr);
 					if(tst == null) throw {message:'no fbml tag'}
 					var fbml = tst[1];
@@ -222,6 +157,7 @@ FGS.ravenwood.Freegifts =
 					
 					params.nextParams = nextParams;
 					FGS.getFBML(params);
+				
 				}
 				catch(err)
 				{
@@ -305,76 +241,12 @@ FGS.ravenwood.Requests =
 				
 				try
 				{
-					if(dataStr.indexOf('You just accepted a neighbor request') != -1)
-					{
-						info.image = '';
-						info.title = 'New neighbour';
-						info.text  = '';
-						info.time = Math.round(new Date().getTime() / 1000);
-						FGS.endWithSuccess(currentType, id, info);
-						return;
-					}
+					var url = $('form[target]', dataHTML).not(FGS.formExclusionString).first().attr('action');
+					var paramTmp = $('form[target]', dataHTML).not(FGS.formExclusionString).first().serialize();
+
 					
-					var el = $('#app_content_120563477996213 > div > div > div > div > div', dataHTML);
-					if($(el).length > 0)
-					{
-						var sendInfo = '';
-						
-						var tmpStr = unescape(currentURL);
-						
-						var pos1 = tmpStr.indexOf('?item_id=');
-						if(pos1 == -1)
-						{
-							pos1 = tmpStr.indexOf('&item_id=');
-						}
-						if(pos1 != -1)
-						{
-							var pos2 = tmpStr.indexOf('&', pos1+1);
-							
-							var giftName = tmpStr.slice(pos1+9,pos2);
-							
-							var pos1 = tmpStr.indexOf('&sender_id=');
-							var pos2 = tmpStr.indexOf('&', pos1+1);
-							
-							var giftRecipient = tmpStr.slice(pos1+11,pos2);			
-								
-							sendInfo = {
-								gift: giftName,
-								destInt: giftRecipient,
-								destName: "Can't say"
-								}
-						}
-						
-						info.thanks = sendInfo;
-						
-						
-						
-						var oldEl = el;
-						
-						var el = $('div:contains("You just accepted this"):last', dataHTML);							
-						
-						if(el.length > 0)
-						{
-							var pos2 = $.trim(el.text()).indexOf(' from ');
-							info.title = $.trim(el.text()).slice(22, pos2);
-							info.image = el.find('img').attr("longdesc");
-						}
-						else
-						{
-							info.title = $.trim(oldEl.text());	
-							info.image = $('#app_content_120563477996213', dataHTML).find('img').attr("longdesc");
-						}
-						
-						info.text  = $.trim(el.text());
-						info.time = Math.round(new Date().getTime() / 1000);
-						
-						FGS.endWithSuccess(currentType, id, info);
-					}
-					else
-					{
-						throw {message: dataStr}
-					}
-				}
+					FGS.fantasy.Requests.Click2(currentType, id, url, paramTmp);
+				} 
 				catch(err)
 				{
 					FGS.dump(err);
@@ -401,5 +273,94 @@ FGS.ravenwood.Requests =
 				}
 			}
 		});
-	}
+	},
+	
+	Click2:	function(currentType, id, currentURL, params, retry)
+	{
+		var $ = FGS.jQuery;
+		var retryThis 	= arguments.callee;
+		var info = {}
+		
+		$.ajax({
+			type: "POST",
+			data: params,
+			url: currentURL,
+			dataType: 'text',
+			success: function(dataStr)
+			{
+				var dataHTML = FGS.HTMLParser(dataStr);
+				
+				try
+				{
+					if(dataStr.indexOf('You just accepted a neighbor request') != -1)
+					{
+						info.image = '';
+						info.title = 'New neighbour';
+						info.text  = '';
+						info.time = Math.round(new Date().getTime() / 1000);
+						FGS.endWithSuccess(currentType, id, info);
+						return;
+					}
+					
+					var tst = new RegExp(/<fb:serverfbml[^>]*?>[\s\S]*?<fb:fbml[^>]*?>([\s\S]*?)<\/fb:fbml>[\s\S]*?<\/fb:serverfbml>/m).exec(dataStr);
+					if(tst != null)
+					{
+						var dataStr = tst[1];
+						dataStr = dataStr.replace(/fb:r/gi, 'fb_r');
+						var dataHTML = FGS.HTMLParser(dataStr);	
+					}
+					
+					var el = $('div:contains("You just accepted this"):last', dataHTML);		
+					
+					if(el.length > 0)
+					{
+						info.image = el.find('img:first').attr('longdesc');
+						
+						info.title = el.text().replace('You just accepted this', '').replace('from ','');
+						info.text  = '';
+						info.time = Math.round(new Date().getTime() / 1000);
+						
+						
+						if($('input[name="item_id"]', dataHTML).length > 0)
+						{
+							info.thanks = 
+							{
+								gift: $('input[name="item_id"]', dataHTML).val()
+							}
+						}
+						
+						FGS.endWithSuccess(currentType, id, info);
+					}
+					else
+					{
+						throw {message: 'no data'}
+					}
+				}
+				catch(err)
+				{
+					FGS.dump(err);
+					FGS.dump(err.message);
+					if(typeof(retry) == 'undefined')
+					{
+						retryThis(currentType, id, currentURL, params, true);
+					}
+					else
+					{
+						FGS.endWithError('receiving', currentType, id);
+					}
+				}
+			},
+			error: function()
+			{
+				if(typeof(retry) == 'undefined')
+				{
+					retryThis(currentType, id, currentURL, params, true);
+				}
+				else
+				{
+					FGS.endWithError('connection', currentType, id);
+				}
+			}
+		});
+	},
 };
